e1256256e54921cbcf085a444218c578ccbacebd
Ivan-the-Gazer <IvanDart1001@outlook.com>
2017-01-03 16:17:25 +0800

    Rem Robot demo（made from Live2d sdk demo）
    Maybe I‘ll try to make my unique
    bot-script(artificial-girlfriend)...(Well,I just learnt how to make a
    live2d module by myself~

---
+++ REadME.txt
@@ -0,0 +1,1 @@
+目前只用chrome测试过，右键chrome快捷方式添加启动参数，就是在目标后面加上一个空格和这一串字符串（启动参数）：--allow-file-access-from-files，然后打开chrome浏览器，把index.html拖进地址栏，就可以查看效果了，直接双击打开index.html可能没法加载刚才那串启动参数。
---
+++ js/RemChatting.js
@@ -0,0 +1,62 @@
+﻿$(document).ready(function () {
+    $('#chat').bind('keypress', function (event) {
+        if (event.keyCode == "13") {
+            replyX();
+        }
+    });
+});
+
+function replyX() {
+    var strX = $("#chat").val();
+    var mBH1 = $("#msgBOX").height();
+    var mBDH1 = $("#msgDeco").offset();
+    var Vurl = "";
+    $.ajax({
+        url:"http://www.tuling123.com/openapi/api",
+        type: "POST",
+        datatype:"JSON",
+        data:{key:"b846dd897d6d4d2fbbfbd8b2cf44736c",info:strX},
+        success:function(returndata){
+            $("#TRUEmsgBOX").html(returndata.text);
+            var mBH2 = $("#TRUEmsgBOX").height();
+            $("#msgBOX").animate({ height: mBH2 });
+            var mBDH2 = mBDH1.top + mBH2 - mBH1;
+            $("#msgDeco").animate({ top: mBDH2 });
+            var tok = "24.1306e1b1c9c6ac5e12418c0b46283aee.2592000.1484458518.282335-8579528";
+            //var tok = "";
+            var lan = "zh";
+            var cuid = "30-3A-64-6E-99-2F"
+            var ctp = "1"
+            var Vword = $("#TRUEmsgBOX")[0].innerHTML;
+            //getTOKEN();
+            Vword = encodeURI(encodeURI(Vword));
+            lan = encodeURI(encodeURI(lan));
+            cuid = encodeURI(encodeURI(cuid));
+            tok = encodeURI(encodeURI(tok));
+            ctp = encodeURI(encodeURI(ctp));
+            Vurl = "http://tsn.baidu.com/text2audio?tex=" + Vword + "&lan=" + lan + "&cuid=" + cuid + "&ctp=" + ctp + "&tok=" + tok;
+            $("#Talk").attr("src", Vurl);
+            $("#Talk")[0].play();
+        },
+        error:function(returndata){
+            console.log(returndata);
+        }
+    });
+}
+
+function getTOKEN() {
+    $.ajax({
+        url: "http://127.0.0.1/getToken",
+        type: "POST",
+        async: false,
+        datatype: "JSON",
+        data: { grant_type: "client_credentials", client_id: "Bk92nRctlvuErcEDAe4YrcxP", client_secret: "1c9ae72a6080707aad6f0d491fa0efca" },
+        success: function (data) {
+            console.log(data);
+            tok = data.token;
+        },
+        error: function (data) {
+            console.log(data);
+        }
+    });
+}
